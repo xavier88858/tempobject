ccc3ff63f7fbd27cccc0b3eae428a488cdf7eb7e
Yang <Yang@yyd.local>
2014-06-12 12:03:42 +0100

    Initial Commit

---
+++ tempobject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  tempobject
+//
+//  Created by Yang on 14-6-12.
+//  Copyright (c) 2014年 Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
