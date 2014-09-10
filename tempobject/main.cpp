163e94b5178faff9e1df3c8f4d11ed77a6038806
xavier88858 <xavier_ly@126.com>
2014-09-10 13:31:12 +0100

    add something
    add something

--- tempobject/main.cpp
+++ tempobject/main.cpp
@@ -1,18 +1,47 @@
-//
-//  main.cpp
-//  tempobject
-//
-//  Created by Yang on 14-6-12.
-//  Copyright (c) 2014年 Yang. All rights reserved.
-//
-
+#include <map>
 #include <iostream>
-
-int main(int argc, const char * argv[])
+#include <string>
+using namespace std;
+class integral;
+class smallint
 {
+public:
+	smallint(int i=0):val(i){cout<<"director"<<endl;}
+	//smallint(integral & );
+	~smallint(){cout<<"dismantle"<<endl;}
+	smallint(const smallint & s):val(s.val){cout<<"copy"<<endl;}
+	operator int() const {return val;}
+	std::size_t val;
+};
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+class integral
+{
+public:
+	integral(int i=10):val(i){cout<<"director"<<endl;}
+	operator smallint() const {return smallint(val%256);}
+	size_t val;
+};
+
+//smallint::smallint(integral &in):val(in.val){cout<<"change"<<endl;}
+
+smallint test()
+{
+	return integral(102);
 }
 
+
+
+
+int main(int argc, char *argv[])
+{
+    integral i1;
+    smallint s1=i1; // 区别以下两个结果
+    cout<<sizeof(int)<<endl;
+
+//    smallint test1=test();
+//     cout<<test1.val<<endl;
+
+    return 0;
+    
+    
+}
